1aad41196d0a25e87dd0009ab4351a2e0c35e4d0
QANaumov <Vlzshark@yandex.ru>
2024-02-18 19:41:09 +0300

    Emojy

--- js.js
+++ js.js
@@ -2,6 +2,4 @@
 const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	alert('Жду в гости')
-})
+button.addEventListener('click', function() {alert('Жду в гости'), alert(emoji = String.fromCodePoint(0x1F970, 0x1F970, 0x1F970))})
